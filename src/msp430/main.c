921701b08f819c3c35e9d925eb9061d0b78b34f6
Shane Volpe <shanevolpe@gmail.com>
2017-03-25 15:31:03 -0400

    Update MSP430 design to have a more advanced command handler capable of taking arguments.

--- src/msp430/main.c
+++ src/msp430/main.c
@@ -1,16 +1,4 @@
-//           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-//                   Version 2, December 2004
 
-//Copyright (C) 2011 Nicol�s Kipreos <nkipreos@gmail.com>
-
-//Everyone is permitted to copy and distribute verbatim or modified
-//copies of this license document, and changing it is allowed as long
-//as the name is changed.
-
-//           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-//  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-// 0. You just DO WHAT THE FUCK YOU WANT TO.
 
 #include <msp430.h>
 #include <stdint.h>
@@ -21,37 +9,76 @@
 //In uniarch there is no more signal.h to sugar coat the interrupts definition, so we do it here
 #define interrupt(x) void __attribute__((interrupt (x)))
 
-#define 	UART_SEL   P1SEL
-#define 	UART_SEL2  P1SEL2
-#define 	UART_DIR   P1DIR
-#define 	UART_IN    P1IN
-#define 	UART_OUT   P1OUT
-#define 	UART_REN   P1REN
+#define     UART_SEL   P1SEL
+#define     UART_SEL2  P1SEL2
+#define     UART_DIR   P1DIR
+#define     UART_IN    P1IN
+#define     UART_OUT   P1OUT
+#define     UART_REN   P1REN
 #define     TXD        BIT2                     // TXD on P1.2
 #define     RXD        BIT1                     // RXD on P1.1
-#define		RELAY	   BIT7						// RELAY on P1.7
-#define		LED1	   BIT0						// RELAY on P1.7
-#define		LED2	   BIT6						// RELAY on P1.7
+#define     RELAY1     BIT0                     // RELAY on P2.0
+#define     RELAY2     BIT1                     // RELAY on P2.1
+#define     RELAY3     BIT2                     // RELAY on P2.2
+#define     RELAY4     BIT3                     // RELAY on P2.3
+#define     LED1       BIT0                     // RELAY on P1.7
+#define     LED2       BIT6                     // RELAY on P1.7
+
+#define     TRUE        1
+#define     FALSE       0
+
+const int RELAY_PIN[] = { BIT0, BIT1, BIT2, BIT3};
+const int LED_PIN[] = { BIT0, BIT6};
+
 
 //Defines to help understand what is attached to each port
 #define LED_DIR P1DIR
 #define LED_OUT P1OUT
+#define RELAY_DIR P2DIR
+#define RELAY_OUT P2OUT
 
+
+typedef enum cmdStatus
+        {
+          waitingCmd,
+          waitingSize,
+          waitingData,
+          received,
+          processed,
+          invalid
+        } cmdStatus;
+
+typedef struct uartCMD {
+   cmdStatus status;
+   uint8_t cmd;
+   uint8_t size;
+   uint8_t received;
+   uint8_t data[9];
+} uartCMD;
+
+typedef struct relayCMD {
+   cmdStatus relay;
+   uint8_t state;
+} relayCMD;
+
+uartCMD _uartCmd;
 
 long tempCalibrated;
 
+
 void timer_init();
 void setup_adc();
-void uart_send_char(char character);
+void uart_send_char(int8_t character);
 void uart_enable();
 void timer_init();
 void timer_start();
+void process_cmd (uartCMD *cmd);
 
-void int_to_str(int nbr, char *res)
+void int_to_str(int nbr, int8_t *res)
 {
   int   div;
   int   len;
-  int   i;
+  unsigned int   i;
 
 
    i = 0;
@@ -78,110 +105,116 @@ void int_to_str(int nbr, char *res)
 
 void sendChar(unsigned char ch)
 {
-	while(!(IFG2&UCA0TXIFG));
-	UCA0TXBUF=ch;
+    while(!(IFG2&UCA0TXIFG));
+    UCA0TXBUF=ch;
 }
 
-void sendStr(char *string)
+void sendStr(int8_t *string)
 {
-	while(!((*string) == '\0'))
-	{
-	sendChar(*string);
-	string++;
-	}
-	sendChar('\r');
-	sendChar('\n');
+    while(!((*string) == '\0'))
+    {
+    sendChar(*string);
+    string++;
+    }
+    sendChar('\r');
+    sendChar('\n');
 }
 
 void led_init()
 {
-	//Set the direction as output
-	LED_DIR |= RELAY;
-	LED_DIR |= LED1;
-	LED_DIR |= LED2;
-	//Start with the red led on and relay off.
-	LED_OUT &= ~RELAY;
+    //Set the directions as output
+    RELAY_DIR |= (RELAY1 | RELAY2 | RELAY3 | RELAY4);
+    LED_DIR |= (LED1 | LED2);
+
+    //Start with the red led on and relay off.
+    RELAY_OUT &= ~(RELAY1 | RELAY2 | RELAY3 | RELAY4);
 }
 
 
 void init()
 {
-	//Stop the watchdog
-	WDTCTL = WDTPW + WDTHOLD;
+    //Stop the watchdog
+    WDTCTL = WDTPW + WDTHOLD;
 
-	BCSCTL1 = 0x07;                    // Set range
-	DCOCTL = 0x4F;
-	  //BCSCTL2 &= ~(DIVS_3);                     // SMCLK = DCO = 1MHz
+    BCSCTL1 = 0x07;                    // Set range
+    DCOCTL = 0x4F;
+      //BCSCTL2 &= ~(DIVS_3);          // SMCLK = DCO = 1MHz
 
-	//Inits the different parts
-	led_init();
-	uart_enable();
-	setup_adc();
-	timer_init();
-	//Enable global interrupts
-	_BIS_SR(GIE);
+    //Inits the different parts
+    led_init();
+    uart_enable();
+    setup_adc();
+    timer_init();
+    //Enable global interrupts
+    _BIS_SR(GIE);
 }
 
 
 int main()
 {
 
-	init();
-	timer_start();
-	//We enter the LPM4 Low Power Mode, which leaves nothing running
-	//This way, the CPU will be off until the button is pressed
-	//_BIS_SR(LPM1_bits);
-	while(1);
-//	return 0;
+    init();
+    timer_start();
+    //We enter the LPM4 Low Power Mode, which leaves nothing running
+    //This way, the CPU will be off until the button is pressed
+    //_BIS_SR(LPM1_bits);
+
+    _uartCmd.status = invalid;
+
+    while(1)
+    {
+        process_cmd(&_uartCmd);
+    }
+//  return 0;
 }
 
 
 void uart_transmit_data(uint8_t length, uint8_t *tx_data)
 {
-	uint8_t current_byte = 0;
-	while (current_byte < length)
-	{
-		uart_send_char(tx_data[current_byte++]);
-	}
-	
+    uint8_t current_byte = 0;
+    while (current_byte < length)
+    {
+        uart_send_char(tx_data[current_byte++]);
+    }
+
 }
 
 
-void uart_send_char(char character)
+void uart_send_char(int8_t character)
 {
-	//uart_transmit_init();
+    //uart_transmit_init();
 
-	while(! (UC0IFG & UCA0TXIFG));
+    while(! (UC0IFG & UCA0TXIFG));
 
-	UCA0TXBUF = character;
+    UCA0TXBUF = character;
 
-	UC0IFG &= ~UCA0TXIFG;
-	//LED_OUT ^= LED_GREEN;
+    UC0IFG &= ~UCA0TXIFG;
+    //LED_OUT ^= LED_GREEN;
 }
 
 void uart_enable()
 {
-	UART_DIR |= TXD;
-	UART_DIR &= ~RXD;
-	UART_SEL |= TXD + RXD;
-	UART_SEL2 |= TXD + RXD;
+    UART_DIR |= TXD;
+    UART_DIR &= ~RXD;
+    UART_SEL |= TXD + RXD;
+    UART_SEL2 |= TXD + RXD;
 
-	UCA0CTL1 |= UCSWRST; //en modo standby mientras se setean datos
-	UCA0CTL1 |= UCSSEL_2; //se elige smclk para el clock (1000000 Hz)
-	//UCABR son 2 bytes separados en los bytes que vienen
-	//Para generar el baudrate querido, mirar la tabla en el users guide
-	// Aca usamos 19230 (Clk/uca0br) = (1000000/52) = 19230
-	UCA0BR0 = 104;
-	UCA0BR1 = 0;
-	UCA0MCTL = UCBRS0; //revisar users guide
-	UCA0CTL1 &= ~UCSWRST;//sacamos del standby
-	IE2 |= UCA0RXIE;//habilitamos la interrupcion de recepcion
+    UCA0CTL1 |= UCSWRST; //en modo standby mientras se setean datos
+    UCA0CTL1 |= UCSSEL_2; //se elige smclk para el clock (1000000 Hz)
+    //UCABR son 2 bytes separados en los bytes que vienen
+    //Para generar el baudrate querido, mirar la tabla en el users guide
+    // Aca usamos 19230 (Clk/uca0br) = (1000000/52) = 19230
+    UCA0BR0 = 104;
+    UCA0BR1 = 0;
+    UCA0MCTL = UCBRS0; //revisar users guide
+    UCA0CTL1 &= ~UCSWRST;//sacamos del standby
+    IE2 |= UCA0RXIE;//habilitamos la interrupcion de recepcion
 }
 
 void setup_adc()
 {
-	//ADC10CTL1 = (0x40 << 8) + 0xF8;
-	//ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON + ADC10IE;
+    //ADC10CTL1 = (0x40 << 8) + 0xF8;
+    //ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON + ADC10IE;
 
     /* disable ADC10 during initialization */
     ADC10CTL0 &= ~ENC;
@@ -246,16 +279,16 @@ void setup_adc()
 
 void timer_init()
 {
-	//Select ACLK, TimerA counter divisor in 1
-	TACTL |= TASSEL_2 + ID_3;
-	//Enables the CCR0 interrupt
-	TACCTL0 |= CCIE;
+    //Select ACLK, TimerA counter divisor in 1
+    TACTL |= TASSEL_2 + ID_3;
+    //Enables the CCR0 interrupt
+    TACCTL0 |= CCIE;
 }
 
 void timer_start()
 {
-	TACTL |= MC_1;
-	TACCR0 = 0x00FF;
+    TACTL |= MC_1;
+    TACCR0 = 0x00FF;
 }
 
 //CCR0 timer interrupt, which toggles the LEDs
@@ -301,47 +334,119 @@ __interrupt void TIMER1_A0_ISR() {
 __interrupt void NMI_ISR() {
 }
 
+void process_cmd (uartCMD *cmd)
+{
+    int8_t temperature[4];
+    uint8_t count, selection, state;
+    if (cmd->status == received)
+    {
+        switch (cmd->cmd)
+        {
+            case 'r': //Set relay
+                if (cmd->size > 2)
+                {
+                    cmd->status = invalid;
+                    break;
+                }
+                selection = cmd->data[0] - '0';
+                state = cmd->data[1] - '0';
+
+                if (selection <= (sizeof(RELAY_PIN)/sizeof(uint8_t)))
+                {
+                    if (state)
+                        RELAY_OUT |= RELAY_PIN[selection];
+                    else
+                        RELAY_OUT &= ~RELAY_PIN[selection];
+                }
+                else
+                    cmd->status = invalid;
+
+                break;
+            case 't': //Get temperature
+
+                tempCalibrated = 0;
+                for (count = 0; count < 10; count++)
+                {
+                        ADC10CTL0 |= ENC + ADC10SC;               // Sampling and conversion start
+                    // Loop until ADC10IFG is set indicating ADC conversion complete
+                        while ((ADC10CTL0 & ADC10IFG) == 0);
+
+                    // Read ADC conversion result from ADC10MEM
+                    //__delay_cycles(1000);
+                        tempCalibrated += ADC10MEM;
+                 }
+                 tempCalibrated /= 10;
+                 int_to_str(tempCalibrated, temperature);
+                 sendStr(temperature);
+                 break;
+
+            case 'l': //Set LED
+                if (cmd->size > 2)
+                {
+                    cmd->status = invalid;
+                    break;
+                }
+                selection = cmd->data[0] - '0';
+                state = cmd->data[1] - '0';
+                if (selection <= (sizeof(LED_PIN)/sizeof(uint8_t)))
+                {
+                    if (state)
+                        LED_OUT |= LED_PIN[selection];
+                    else
+                        LED_OUT &= ~LED_PIN[selection];
+                }
+                break;
+            default:
+                cmd->status = invalid;
+                break;
+
+        }
+        if (cmd->status != invalid)
+            cmd->status = processed;
+    }
+}
+
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void UART_RCV_ISR()
 {
-	uint8_t rx_byte = UCA0RXBUF;
-	char temperature[4];
-	if(rx_byte == 'b')
-	{
-		LED_OUT |= RELAY;
-	}
-	else if(rx_byte == 'o')
-	{
-		LED_OUT &= ~RELAY;
-	}
-	else if(rx_byte == 'a')
-	{
-		// ADC Start Conversion - Software trigger
-//		ADC10CTL0 |= ADC10SC;
-		unsigned char count;
-		tempCalibrated = 0;
-		for (count = 0; count < 10; count++)
-		{
-			ADC10CTL0 |= ENC + ADC10SC;               // Sampling and conversion start
-		// Loop until ADC10IFG is set indicating ADC conversion complete
-			while ((ADC10CTL0 & ADC10IFG) == 0);
+    uint8_t rx_byte = UCA0RXBUF;
+    uint8_t size;
 
-		// Read ADC conversion result from ADC10MEM
-		//__delay_cycles(1000);
-			tempCalibrated += ADC10MEM;
-		}
-		tempCalibrated /= 10;
-		//tempCalibrated *= 1.5/(2^10);
-  		int_to_str(tempCalibrated, temperature);
-  		sendStr(temperature);
-		//printfssv("%i\n\r",tempCalibrated);
-	}
-	else if(rx_byte == 'g')
-	{
-		LED_OUT |= LED1;
-	}
-	else if(rx_byte == 'd')
-	{
-		LED_OUT &= ~LED1;
-	}
+
+    switch (_uartCmd.status)
+    {
+        case processed:
+        case invalid:
+            if (rx_byte == 'c')
+                _uartCmd.status = waitingCmd;
+            else
+                _uartCmd.status = invalid;
+            break;
+        case waitingCmd:
+            _uartCmd.cmd = rx_byte;
+            _uartCmd.status = waitingSize;
+            break;
+        case waitingSize:
+
+            size = rx_byte - '0';
+            if ((size >= 0) && (size < 10))
+            {
+                _uartCmd.size = size;
+                _uartCmd.received = 0;
+                _uartCmd.status = waitingData;
+                if (size == 0)
+                    _uartCmd.status = received;
+
+            }
+            else
+                _uartCmd.status = invalid;
+            break;
+        case waitingData:
+            _uartCmd.data[_uartCmd.received++] = rx_byte;
+
+            if (_uartCmd.received == _uartCmd.size)
+                _uartCmd.status = received;
+           break;
+
+    }
 }
